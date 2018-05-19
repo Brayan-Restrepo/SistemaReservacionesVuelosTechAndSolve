7ca5768f19e20cfefd18fad709850721a79fbaf7
Brayan.Restrepo.VC <desarrollo8@virtualclors.com>
2018-05-19 12:56:20 -0500

    Inicio proyecto

---
+++ src/main/java/com/techandsolve/SistemaReservacionesVuelosTechAndSolveApplication.java
@@ -0,0 +1,12 @@
+package com.techandsolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReservacionesVuelosTechAndSolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReservacionesVuelosTechAndSolveApplication.class, args);
+	}
+}
